a3cf4f73372a593c46f6c9524cc1ce1023699235
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-09-21 10:10:13 -0700

    pushed seed data into tables

--- seeds/seed.leafdesk_tables.sql
+++ seeds/seed.leafdesk_tables.sql
@@ -11,15 +11,15 @@ VALUES
     ('joeb', 'joebpassword', 'joeb@email.com'),
     ('josht', 'joshtpassword', 'josht@email.com');
 
-INSERT INTO incidents (title, comments, users_id, inc_pri, Office_Loc)
+INSERT INTO incidents (title, comments, users_id, inc_pri, office_location)
 VALUES
     ('software', 'need update', '1', 'Low', 'Bremerton'),
-    ('hardware', 'camera does work', '2', 'MED', 'Bremerton'),
+    ('hardware', 'camera does work', '2', 'Med', 'Bremerton'),
     ('other', 'displays are not showing up', '1', 'High', 'Camp Murray'),
     ('software', 'need sketch software', '1', 'High', 'Camp Murray'),
-    ('hardware', 'need a mobile phone', '2', 'MED', 'Renton');
+    ('hardware', 'need a mobile phone', '2', 'Med', 'Renton');
 
-INSERT INTO employees (fname, lname, email, emp_address, emp_ST, emp_Zip, office_Loc, emp_Roles)
+INSERT INTO employees (fname, lname, email, emp_address, emp_st, emp_zip, office_location, emp_roles)
 VALUES
     ('jack', 'state', 'jackstate@email.com', '830 armor drive', 'WA', '98430', 'Camp Murray', 'Admin'),
     ('jason', 'borne', 'jasonborne@email.com', '123 street drive', 'WA', '98337', 'Bremerton', 'Tech');
